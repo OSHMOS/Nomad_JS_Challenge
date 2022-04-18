b6889aa858b0543335644961f3f6cf08916696bb
OSHMOS <ojames9707@gmail.com>
2022-04-18 17:30:29 +0900

    노마드 JS 챌린지 : 범위에 음수 값이 포함되면 안됩니다. 간단명료하게.

--- 4월 18일, 19일/src/index.js
+++ 4월 18일, 19일/src/index.js
@@ -1,20 +1,14 @@
 let range = document.querySelector("#range");
 const yourNum = document.querySelector("#yourNum");
-const btn = document.querySelector("button");
+const btn = document.querySelector("#playButton");
 const situation = document.querySelector("#situation");
 const result = document.querySelector("#result");
 
-function judgePlus() {
-  if (parseInt(range.value, 10) > 0) {
-    range = parseInt(range.value, 10);
-  } else {
-    alert("자동으로 -1을 곱해서 양수로 변환합니다.");
-    range = parseInt(range.value, 10) * -1;
-  }
-}
-
 function gameStart(e) {
   e.preventDefault();
+  if (parseInt(range.value, 10) < 0) {
+    range = parseInt(prompt("양수를 입력하세요."), 10);
+  }
   let compNum = Math.ceil(Math.random() * range);
   situation.innerHTML = `You chose: 
   ${parseInt(yourNum.value, 10)}, 
@@ -26,5 +20,4 @@ function gameStart(e) {
   }
 }
 
-range.addEventListener("mouseleave", judgePlus);
 btn.addEventListener("click", gameStart);
