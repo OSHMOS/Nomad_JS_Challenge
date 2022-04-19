161e3eaf64b8abc61c9278b818ab68083760a3e2
OSHMOS <ojames9707@gmail.com>
2022-04-20 02:23:48 +0900

    노마드 JS 챌린지 :  input 태그에 min 속성 안 쓰고, if ~ else 문으로 해결하기

--- 4월 18일, 19일/src/index.js
+++ 4월 18일, 19일/src/index.js
@@ -1,13 +1,15 @@
-let range = document.querySelector("#range");
 const yourNum = document.querySelector("#yourNum");
-const btn = document.querySelector("#playButton");
+const btn = document.querySelector("button");
 const situation = document.querySelector("#situation");
 const result = document.querySelector("#result");
 
 function gameStart(e) {
   e.preventDefault();
+  let range = document.querySelector("#range");
   if (parseInt(range.value, 10) < 0) {
     range = parseInt(prompt("양수를 입력하세요."), 10);
+  } else {
+    range = parseInt(range.value, 10);
   }
   let compNum = Math.ceil(Math.random() * range);
   situation.innerHTML = `You chose: 
